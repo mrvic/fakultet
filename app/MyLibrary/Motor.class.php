5276dc4d7e9a9fff842b2cbd1031a82b937ceae8
pmrvic <pmrvic@partedmagic.data.centar>
2016-11-25 13:51:03 +0100

    Dodana vlastita klasa u app/MyLibrary

---
+++ app/MyLibrary/Motor.class.php
@@ -0,0 +1,38 @@
+<?php
+namespace App\MyLibrary;
+
+/**
+ * prije pokretanja testova u composer.json treba dodati "app/myLibrary"
+ * to je direktorij gdje imamo vlastite klase:
+ "autoload": {
+        "classmap": [
+            "database",
+            "app/myLibrary"
+        ],
+        "psr-4": {
+            "App\\": "app/"
+        }
+    }
+ *
+ * @author pmrvic
+ */
+class Motor {
+    //put your code here    
+    public $status="\n\t\t------    Auto 1 alive!   -------\n\n";
+    public $zvuk="brmmmmmm";
+    public function __construct($s){
+        // Cilj je testirati namjerno ubacivanjem krivog tipa varijable
+        // kako bi provocirali hvatanje greške u 
+        // /tests/Osnove/motorClassTest@testPokusajDaKreiramoMotorSaKrivimArgumentima()
+        if (!is_numeric($s)){ 
+            // mora se postaviti \ znak jer je to root klasa a ne custom \InvalidArgumentException
+            throw new \InvalidArgumentException("Moraš ubaciti broj!");        
+        }
+        else{
+        echo "\nMotor je kreiran sa ispravno unešenim argumentom\n";
+        $this->zvuk=$s;
+        }
+    }
+
+}
+
