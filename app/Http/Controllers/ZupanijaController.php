e50dde35b787e1dfef6b466738a54ad1caadd1d1
pmrvic <pmrvic@partedmagic.data.centar>
2016-11-25 13:54:44 +0100

    Dodani kontroleri Mjesto, Stud, Zupanija i auto

---
+++ app/Http/Controllers/ZupanijaController.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace App\Http\Controllers;
+use Request;
+use App\Http\Requests;
+use Validator;
+use Input;
+use Session;
+use Redirect;
+use View;
+use Zupanija;
+use App\Item;
+
+
+//use Illuminate\Http\Request;
+
+class ZupanijaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $zupanijas = Zupanija::all();
+        // load the view and pass the nerds
+            return View::make('fakultet.zupanija.index')
+                            ->with('zupanijas', $zupanijas);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return View::make('fakultet.zupanija.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+           // validate
+        // read more on validation at http://laravel.com/docs/validation
+        $rules = array(
+            'sifZupanija'=>'required',
+            'nazZupanija' => 'required'
+        );
+        $validator = Validator::make(Input::all(), $rules);
+
+        //$validator = Validator::make($request->all(), $rules)->validate();
+        // process the login
+        if ($validator->fails()) {
+            return Redirect::to('zupanija/create')
+                            ->withErrors($validator)
+                            ->withInput(Input::except('password'));
+        } else {
+            // store
+            $zupanija = new Zupanija;
+            $zupanija->sifZupanija=Input::get('sifZupanija');
+            $zupanija->nazZupanija = Input::get('nazZupanija');
+            $zupanija->save();
+
+            // redirect
+            Session::flash('message', 'Successfully created zupanija!');
+            return Redirect::to('zupanija');
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        // get the nerd
+        $zupanija = Zupanija::find($id);
+
+        // show the view and pass the nerd to it
+        return View::make('fakultet.zupanija.show')
+                        ->with('zupanija', $zupanija);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $zupanija = Zupanija::find($id);
+
+        // show the edit form and pass the nerd
+        return View::make('fakultet.zupanija.edit')
+                        ->with('zupanija', $zupanija);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+                // validate
+        // read more on validation at http://laravel.com/docs/validation
+        $rules = array(
+            'sifZupanija' => 'required|numeric',
+            'nazZupanija' => 'required'
+        );
+        $validator = Validator::make(Input::all(), $rules);
+
+        // process the login
+        if ($validator->fails()) {
+            return Redirect::to('zupanija/' . $id . '/edit')
+                            ->withErrors($validator)
+                            ->withInput(Input::except('password'));
+        } else {
+            // store
+            $zupanija = Zupanija::find($id);
+            $zupanija ->sifZupanija = Input::get('sifZupanija');
+            $zupanija ->nazZupanija = Input::get('nazZupanija');
+            $zupanija->save();
+
+            // redirect
+            Session::flash('message', 'Successfully updated zupanija!');
+            return Redirect::to('zupanija');
+        }
+        
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        // delete
+        $zupanija = Zupanija::find($id);
+        $zupanija->delete();
+
+        // redirect
+        Session::flash('message', 'Successfully deleted the Županija!');
+        return Redirect::to('zupanija');
+    }
+}
