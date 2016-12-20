eece4473b8fd884dc602fd076fdd002db07514d2
pmrvic <mrvic.predrag@gmail.com>
2016-12-20 16:32:13 +0100

    Dodan kompletan UvodPHP

---
+++ public/UvodPHP/17-20/4.2ifelsegrananje.php
@@ -0,0 +1,27 @@
+<?php
+
+$naziv="alg";
+if(strlen($naziv)>=5){
+
+echo "Odlično!";
+}
+else{
+
+if(strlen($naziv)>=3 &&  strlen($naziv)<=4){
+	echo "Ajde ok je";
+}
+
+
+else {
+	echo "ništa od igrica";
+}
+}
+
+echo "<hr>Skraceni oblici<br>";
+
+$a=6;
+$b=($a>5)?"ispunjen uvjet":"nije ispunjen"; 
+//<uvijet>?TRUE:FALSE
+echo $b;
+
+?>
