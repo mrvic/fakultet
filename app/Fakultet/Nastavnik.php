1e2eda3b0b8a1bf59a6bfaa5a1000711ec10a14a
pmrvic <mrvic.predrag@gmail.com>
2017-05-22 22:12:37 +0200

    kreiran model Nastavnik

---
+++ app/Fakultet/Nastavnik.php
@@ -0,0 +1,13 @@
+<?php
+//php artisan make:model Nastavnik
+namespace Fakultet;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Nastavnik extends Model
+{
+    protected $table    = 'nastavnik';
+    protected $primaryKey='sifNastavnik';
+    
+    
+}
