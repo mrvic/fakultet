6def43cf2b0f328ede020b54b9b076de537492ea
zilicic <zilicic5@h-1.hr>
2017-06-02 13:24:31 +0200

    Ispravljene greske Nastavnik i NastavnikController kod pusha od 31052017

--- app/Fakultet/Nastavnik.php
+++ app/Fakultet/Nastavnik.php
@@ -1,20 +1,11 @@
 <?php
-<<<<<<< HEAD
-//php artisan make:model Nastavnik --migration
-=======
 //php artisan make:model Nastavnik
->>>>>>> 1e2eda3b0b8a1bf59a6bfaa5a1000711ec10a14a
 namespace Fakultet;
-
 use Illuminate\Database\Eloquent\Model;
-
 class Nastavnik extends Model
 {
     protected $table    = 'nastavnik';
     protected $primaryKey='sifNastavnik';
-<<<<<<< HEAD
-=======
     
     
->>>>>>> 1e2eda3b0b8a1bf59a6bfaa5a1000711ec10a14a
 }
