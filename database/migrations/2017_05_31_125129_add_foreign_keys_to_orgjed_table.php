8ce2ff432f95787171baeb1f490de9fc09dd12bc
pmrvic <mrvic.predrag@gmail.com>
2017-06-05 17:31:25 +0200

    Prosli svi dusk testovi

--- database/migrations/2017_05_31_125129_add_foreign_keys_to_orgjed_table.php
+++ database/migrations/2017_05_31_125129_add_foreign_keys_to_orgjed_table.php
@@ -4,32 +4,34 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddForeignKeysToOrgjedTable extends Migration
-{
-   /**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::table('orgjed', function(Blueprint $table)
-		{
-			$table->foreign('sifNadorgjed', 'orgjed_ibfk_1')->references('sifOrgjed')->on('orgjed')->onUpdate('RESTRICT')->onDelete('RESTRICT');
-		});
-	}
+class AddForeignKeysToOrgjedTable extends Migration {
 
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::table('orgjed', function(Blueprint $table) {
+            DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+            $table->foreign('sifNadorgjed', 'orgjed_ibfk_1')
+                  ->references('sifOrgjed')
+                  ->on('orgjed')
+                  ->onUpdate('RESTRICT')
+                  ->onDelete('RESTRICT');
+            DB::statement('SET FOREIGN_KEY_CHECKS = 1');
+        });
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::table('orgjed', function(Blueprint $table)
-		{
-			$table->dropForeign('orgjed_ibfk_1');
-		});
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::table('orgjed', function(Blueprint $table) {
+            $table->dropForeign('orgjed_ibfk_1');
+        });
+    }
+
 }
