6e8f543a62417e13a6e93ded282db11622fd1bf0
pmrvic <pmrvic@partedmagic.data.centar>
2017-12-07 10:02:55 +0100

    merge StudTest i ZupanijaTest

---
+++ tests/Browser/StudTest.php
@@ -0,0 +1,120 @@
+<?php
+
+use Tests\DuskTestCase;
+use Laravel\Dusk\Chrome;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Faker\Factory as Faker;
+
+//php artisan make:test StudTest
+//php artisan dusk .\tests\4.NaprednoPHP\StudTest.php
+//class StudTest extends TestCase {
+class StudTest extends DuskTestCase {
+
+    public function testProvjeriViewStud() {
+        $this->assertTrue(view()->exists('fakultet.student.index')
+                , 'Kreiraj view "\resources\views\fakultet\student\index.blade.php"');
+
+        $this->assertTrue(view()->exists('fakultet.student.edit')
+                , 'Kreiraj view "\resources\views\fakultet\student\edit.blade.php"');
+
+        $this->assertTrue(view()->exists('fakultet.student.show')
+                , 'Kreiraj view "\resources\views\fakultet\student\show.blade.php"');
+
+        $this->assertTrue(view()->exists('fakultet.student.create')
+                , 'Kreiraj view "\resources\views\fakultet\student\create.blade.php"');
+    }
+
+    public function testPogledajJeliSeUcitalaListaStudenata() {
+        $this->browse(function ($browser) {
+            $browser->visit('/studenti')
+                    ->assertSee('Svi studenti')
+                    ->assertSee('Daniel Kamenar')
+                    ->assertDontSee('Ovo je string koji ne smije biti na stranici');
+        });
+        /*
+          $this->visit('/studenti')
+          ->see('Svi studenti')
+          ->see('Slavko Prstačić')
+          ->dontSee('Ovo je string koji ne smije biti na stranici');
+         */
+    }
+
+    
+    /**
+     * 
+     * 
+     * 
+     */
+    
+public function testStudentCRUD() {
+    static::markTestSkipped('');  // OVAJ TEST RADI LOKALNO ALI NEĆE NA TRAVISU
+    $this->browse(function ($browser) {
+        $faker = Faker::create();
+        $student = new \Fakultet\Stud;
+
+        $student->imeStud = $faker->name;
+        $student->prezStud = $faker->lastName;
+        $student->pbrRod = \Fakultet\Mjesto::all()->first()->pbr;
+        $student->pbrStan = \Fakultet\Mjesto::all()->random()->pbr;
+        
+        /* 
+// DUSK javlja grešku: 
+Illuminate\Support\ViewErrorBag Object ( [bags:protected] => Array ( [default] => Illuminate\Support\MessageBag Object ( [messages:protected] => Array ( [datRodStud] => Array ( [0] => The dat rod stud does not match the format Y-n-j. ) ) [format:protected] => :message ) ) ) 1
+         */
+       // $student->datRodStud = $faker->date('Y-n-j');//date('Y-m-d');
+        $student->datRodStud = '1984-12-15 00:00:00';
+        $student->jmbgStud = 123456789;
+        // Prvo kreiramo studenta da nam dodjeli autoincrement mbrStud
+        $student->slikaStud = 0; // Input::get('slikaStud');
+
+        $student->save();
+
+        $browser->visit('/studenti')
+                ->clickLink('Pokaži ovog studenta')
+                ->assertPathIs('/studenti/' . $student->mbrStud)
+                ->assertSee($student->prezStud)
+                ->clickLink('Uredi ovog studenta')
+                ->assertPathIs('/studenti/' . $student->mbrStud . '/edit')
+                ->type('jmbgStud',1501984330075)
+                ->press('Uredi studenta')
+                ->pause(3000)
+                ->assertPathIs('/studenti')
+                ->clickLink('Uredi ovog studenta')
+                ->pause(3000)
+                ->assertPathIs('/studenti/' . $student->mbrStud . '/edit')
+                // provjera vrijednosti polja forme
+                ->assertInputValue('jmbgStud', 1501984330075) 
+                ->pause(3000)
+                ->type('jmbgStud',1501984330074)
+                ->press('Uredi studenta')
+                ->assertPathIs('/studenti');
+
+        $student->delete();
+    });
+// Resetira PRIMARNI KLJUČ mbrStud kako nebi rastao kako dodajemo i brisemo studente
+        DB::statement("ALTER TABLE `stud` AUTO_INCREMENT = 1535;");
+    }
+
+}
+
+/*
+  // stari phpunit nacin testiranja
+ * 
+  $this->visit('/studenti')
+  ->click('Pokaži ovog studenta')
+  ->seePageIs('/studenti/'.$student->mbrStud)
+  ->see($student->prezStud)
+  ->click('Uredi ovog studenta')
+  ->seePageIs('/studenti/'.$student->mbrStud.'/edit')
+  ->type(1501984330075, 'jmbgStud')
+  ->press('Uredi studenta')
+  ->seePageIs('/studenti')
+  ->click('Uredi ovog studenta')
+  ->seePageIs('/studenti/'.$student->mbrStud.'/edit')
+  ->see(1501984330075)
+  ->type(1501984330074, 'jmbgStud')
+  ->press('Uredi studenta')
+  ->seePageIs('/studenti');
+ */
---
+++ tests/Browser/ZupanijaTest.php
@@ -0,0 +1,122 @@
+<?php
+
+//use Illuminate\Foundation\Testing\WithoutMiddleware;
+//use Illuminate\Foundation\Testing\DatabaseMigrations;
+//use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+
+use Faker\Factory as Faker;
+use Fakultet\Zupanija;
+use Tests\DuskTestCase;
+use Laravel\Dusk\Chrome;
+
+//use Fakultet;
+
+class ZupanijaTest extends DuskTestCase {
+
+    public function testModelZupanija() {
+        //$lista_mjesta->Zupanija::find(1)->mjesto;
+        //print_r($lista_mjesta);
+        //TODO $lista_mjesta->Zupanija::find(1)->mjesto;
+        /*
+          $z= new Zupanija();
+          echo "\n".get_class($this).": ispis Zupanije()\n";
+          print_r( $z->fillable);
+         */
+
+        $this->assertArraySubset(
+                (new Zupanija())->fillable
+                , array('sifZupanija', 'nazZupanija')
+                , 'Županija mora imati $fillable=["sifZupanija","nazZupanija"] ');
+    }
+
+    /**
+     * Potrebno je kreirati view-ove unutar:
+     * \resources\views\fakultet\zupanija\
+     *
+     * @return void
+     */
+    public function testProvjeriViewZupanija() {
+        $this->assertTrue(view()->exists('fakultet.zupanija.index')
+                , 'Kreiraj view "\resources\views\fakultet\zupanija\index.blade.php"');
+
+        $this->assertTrue(view()->exists('fakultet.zupanija.create')
+                , 'Kreiraj view "\resources\views\fakultet\zupanija\create.blade.php"');
+
+        $this->assertTrue(view()->exists('fakultet.zupanija.edit')
+                , 'Kreiraj view "\resources\views\fakultet\zupanija\edit.blade.php"');
+
+        $this->assertTrue(view()->exists('fakultet.zupanija.show')
+                , 'Kreiraj view "\resources\views\fakultet\zupanija\show.blade.php"');
+    }
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testPogledajJeliSeUcitalaZupanijaLista() {
+        //TODO Treba dopuniti test za Županije
+        //static::markTestSkipped('');
+        $this->browse(function ($browser) {
+            $browser->visit('/zupanija')
+                    //->assertSee('Sve županije')
+                    ->assertSee('Sve županije')    // ne prepoznaje hr slova?    
+                    ->assertdontSee('Ovo je string koji ne smije biti na stranici');
+        });
+    }
+
+    public function testKlikniNaPokaziZupaniju() {
+        $this->browse(function ($browser) {
+            $browser->visit('/zupanija')
+                    //->click('zupanija-0')  // neće jer nije text linka
+                    // mora ovao ako je link ima ID
+                    ->visit($browser->attribute('#zupanija-0', 'href'))
+                    ->assertPathIs('/zupanija/0');
+        });
+    }
+
+    public function testKreirajNovo() {
+        //static::markTestSkipped('');
+        
+        $this->browse(function ($browser) {
+            $faker = Faker::create();
+            $browser->visit('/zupanija/create')
+                    //->type("$faker->randomElement(33, 44, 55, 77, 99)", 'sifZupanija')
+                    ->type('sifZupanija',"99")  // testiramo brojem 99
+                    ->type( 'nazZupanija',"_test_ $faker->city")
+                    //->click($browser->attribute('zupanija-dodaj'))
+                    ->click('#zupanija-dodaj')
+                    //->seePageIs('/zupanija');
+                    //->onPage('/zupanija');
+                    ->assertPathIs('/zupanija');
+        });
+    }
+    
+    
+    
+    
+     /**
+      * JAVLJA GRESKU NA TRAVISU PA GA SKIPAM, LOKALNO RADI???? 
+      * 1) ZupanijaTest::testPokusajObrisati
+Facebook\WebDriver\Exception\NoSuchElementException: no such element: Unable to locate element: {"method":"id","selector":"zupanija-del-99"}
+  (Session info: chrome=60.0.3112.113)
+  (Driver info: chromedriver=2.28.455506 (18f6627e265f442aeec9b6661a49fe819aeeea1f),platform=Linux 4.4.0-93-generic x86_64)
+      * 
+      * 
+      * 
+      * 
+      * 
+     * @depends testKreirajNovo
+     */
+    public function testPokusajObrisati() {
+          static::markTestSkipped('');
+        $this->browse(function ($browser) {
+            $browser->visit('/zupanija')
+                    //->press('Delete this zupanija')
+                    ->click('#zupanija-del-99')
+                    ->assertSee('Successfully deleted!');
+        });
+    }
+
+}
